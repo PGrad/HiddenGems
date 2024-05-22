3c74229036dea614f383e79f97084ced622dbdb9
Paul Grad <paulgrad3rd@gmail.com>
2024-05-22 16:22:12 -0700

    Build 2 index html files
    I have the vite setup now that
    it will build separate index.html
    files for each language.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url';
 import vue from '@vitejs/plugin-vue'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/HiddenGems/"
+  base: "/HiddenGems/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        es: resolve(__dirname, 'es/index.html'),
+      },
+    },
+  },
 })
