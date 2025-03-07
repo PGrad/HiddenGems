fe8483ae591fa51dae21882ccbf8fae50f788114
Paul Grad <paulgrad3rd@gmail.com>
2025-03-07 13:16:58 -0800

    Move the suggestion down for mobile

--- src/App.vue
+++ src/App.vue
@@ -288,8 +288,8 @@ function goToUrl(url: string) {
         <span class="first">H</span>idden <span class="second">G</span>ems
       </h1>
       <Search v-if="loggedIn && avatarUrl !== ''" v-model:prompt-value="promptValue" :search-handler="debouncedSearch" />
-      <h3 v-if="currentArtist !== '' && songs === null" class="inline-flex justify-between gap-1 m-2 text-lg w-100 whitespace-nowrap overflow-hidden text-ellipsis animate-bounce">
-        Find rare songs from <span class="artist-name" >{{ currentArtist }}</span>
+      <h3 v-if="currentArtist !== '' && songs === null" class="mt-6 inline-flex flex-col justify-between gap-1 m-2 text-lg">
+        Find rare songs from <span class="animate-bounce artist-name w-60 md:w-100 whitespace-nowrap overflow-hidden text-ellipsis" >{{ currentArtist }}</span>
       </h3>
       <button v-else @click="makePlaylist" class="btn btn-green-shadow text-lg flex items-center justify-between gap-2 mt-2">
         <img src="./assets/spotify_icon.svg" class="w-10" />
